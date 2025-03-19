9fe42931422f6883952c22715f842d85f4aa8fc3
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-19 16:14:31 +0000

    Create zendebinstaller.sh

---
+++ zendebinstaller.sh
@@ -0,0 +1,15 @@
+echo "avaliable tag versions: "
+echo "1. 0.0.0-a.2 (alpha)"
+echo "2. 1.0.0-a.2 (alpha)"
+read -p "enter a number containing a tag version (ex: 0.0.0-a.2): " tag
+if [ "$tag" = "1" ]; then
+wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/0.0.0-a.2/zen.linux.deb && sudo apt install ./zen.linux.deb 
+echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
+sudo rm -rf ~/zen.linux.deb
+elif [ "$tag" = "2" ]; then 
+wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.2/zen.linux.deb && sudo apt install ./zen.linux.deb 
+echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
+sudo rm -rf ~/zen.linux.deb
+else
+echo "invalid input, exiting..."
+fi
