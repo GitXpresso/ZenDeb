e0dd9e50859d7fe9a00402e121e00b2b76723b52
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-20 12:22:11 +0000

    Update zendebinstaller.sh
    added 1.0.0-a.3

--- zendebinstaller.sh
+++ zendebinstaller.sh
@@ -1,7 +1,8 @@
 echo "avaliable tag versions: "
 echo "1. 0.0.0-a.2 (alpha)"
-echo "1.0.0-a.1" (alpha)"
-echo "2. 1.0.0-a.2 (alpha)"
+echo "2.0.0-a.1" (alpha)"
+echo "3. 1.0.0-a.2 (alpha)"
+echo "4. 1.0.0-a.3 (alpha)"
 read -p "enter a number containing a tag version (ex: 1 2 3 etc.): " tag
 if [ "$tag" = "1" ]; then
 wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/0.0.0-a.2/zen.linux.deb && sudo apt install ~/zen.linux.deb 
@@ -13,6 +14,10 @@ echo "uninstalling zen.linux.deb to avoid conflicts when installing a different 
 sudo rm -rf ~/zen.linux.deb
 elif [ "$tag" = "3" ]; then
 wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.1/zen.linux.deb && sudo apt install ~/zen.linux.deb 
+echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
+sudo rm -rf ~/zen.linux.deb
+elif [ "$tag" = "4" ]; then
+wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.3/zen.linux.deb && sudo apt install ~/zen.linux.deb
 echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
 sudo rm -rf ~/zen.linux.deb
 else
