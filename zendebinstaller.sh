a6d7f4f9d8c1ca9737a49a0d1a7f7ce04666739f
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-21 12:33:15 +0000

    Update zendebinstaller.sh

--- zendebinstaller.sh
+++ zendebinstaller.sh
@@ -6,6 +6,8 @@ echo "1. 0.0.0-a.2 (alpha)"
 echo "2. 1.0.0-a.1 (alpha)"
 echo "3. 1.0.0-a.2 (alpha)"
 echo "4. 1.0.0-a.3 (alpha)"
+echo "5. 1.0.0-a.4 (alpha)"
+echo "6. 1.0.0-a.6 (alpha)"
 read -p "enter a number containing a tag version (ex: 1 2 3 etc.): " tag
 if [ "$tag" = "1" ]; then
 wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/0.0.0-a.2/zen.linux.deb && sudo apt install ~/zen.linux.deb 
@@ -21,6 +23,14 @@ echo "uninstalling zen.linux.deb to avoid conflicts when installing a different 
 sudo rm -rf ~/zen.linux.deb
 elif [ "$tag" = "4" ]; then
 wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.3/zen.linux.deb && sudo apt install ~/zen.linux.deb
+echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
+sudo rm -rf ~/zen.linux.deb
+elif [ "$tag" = "5" ]; then
+wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.4/zen.linux.deb && sudo apt install ~/zen.linux.deb
+echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
+sudo rm -rf ~/zen.linux.deb
+elif [ "$tag" = "6" ]; then
+wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.6/zen.linux.deb && sudo apt install ~/zen.linux.deb
 echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
 sudo rm -rf ~/zen.linux.deb
 else
@@ -36,6 +46,8 @@ echo "1. 0.0.0-a.2 (alpha)"
 echo "2. 1.0.0-a.1 (alpha)"
 echo "3. 1.0.0-a.2 (alpha)"
 echo "4. 1.0.0-a.3 (alpha)"
+echo "5. 1.0.0-a.4 (alpha)"
+echo "6. 1.0.0-a.6 (alpha)"
 read -p "enter a number containing a tag version (ex: 1 2 3 etc.): " tag
 if [ "$tag" = "1" ]; then
 wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/0.0.0-a.2/zen.linux.deb && sudo apt install ~/zen.linux.deb 
@@ -51,6 +63,14 @@ echo "uninstalling zen.linux.deb to avoid conflicts when installing a different 
 sudo rm -rf ~/zen.linux.deb
 elif [ "$tag" = "4" ]; then
 wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.3/zen.linux.deb && sudo apt install ~/zen.linux.deb
+echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
+sudo rm -rf ~/zen.linux.deb
+elif [ "$tag" = "5" ]; then
+wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.4/zen.linux.deb && sudo apt install ~/zen.linux.deb
+echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
+sudo rm -rf ~/zen.linux.deb
+elif [ "$tag" = "6" ]; then
+wget -P ~/ https://github.com/GitXpresso/ZenDeb/releases/download/1.0.0-a.6/zen.linux.deb && sudo apt install ~/zen.linux.deb
 echo "uninstalling zen.linux.deb to avoid conflicts when installing a different version of zen browser"
 sudo rm -rf ~/zen.linux.deb
 else
